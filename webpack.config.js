12b576f445966f3ca003ded7e7e92f47dfc5372a
Brian <brpereir@ucsc.edu>
2021-03-01 23:51:42 -0800

    setting up webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// entry -> output
+
+
+// https://webpack.js.org/concepts/
+
+const path = require('path');
+
+module.exports = {
+    // where it should start
+    entry: "./src/app.js",
+    output: {
+        // join function to connec the absolute path
+        path: path.join(__dirname, 'public'),
+        // all the scripts into this bundle
+        filename: 'bundle.js'
+    }
+
+};
