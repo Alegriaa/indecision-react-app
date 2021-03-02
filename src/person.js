217bab4e77d98f9890a5e3196c0659b218074795
Brian <brpereir@ucsc.edu>
2021-03-02 00:37:17 -0800

    ES6 import/export w/ webpack

---
+++ src/person.js
@@ -0,0 +1,16 @@
+export const isAdult = (num) => {
+    if (num >= 18) {
+        return true;
+    }
+    return false;
+
+};
+
+
+export const canDrink = (num) => {
+    if (num >= 21) {
+        return true;
+    }
+    return false;
+
+};
