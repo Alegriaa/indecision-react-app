da7c591fc75e649dd97f261e8cf9ef0458fb7538
Brian <brpereir@ucsc.edu>
2021-03-06 01:34:32 -0800

    styling option items

--- src/components/Options.js
+++ src/components/Options.js
@@ -13,10 +13,11 @@ const Options = (props) => (
         
             {props.options.length === 0  && <p className="widget__message">Please add a option to begin</p>}
             {
-                props.options.map((option) => (
+                props.options.map((option, index) => (
                 <Option 
                 key ={option} 
                 optionText ={option} 
+                count={index + 1}
                 handleDeleteOption ={props.handleDeleteOption}
                 />
                 ))
