8221d36637323cc96a53bda1df97d98cb7c3d301
Brian <brpereir@ucsc.edu>
2021-03-05 23:42:16 -0800

    scss for buttons

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,7 +3,10 @@ import React from 'react';
 import Option from './Option.js'
 const Options = (props) => (
         <div>
-            <button onClick = {props.handleDeleteOptions}>Remove All</button>
+            <button 
+            // adding modified button scss here
+            className="button button--link"
+            onClick = {props.handleDeleteOptions}>Remove All</button>
             {props.options.lnegth === 0  && <p>Please add a option to begin</p>}
             {
                 props.options.map((option) => (
